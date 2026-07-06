0ee29e35251ade502f34a201582214ee3e0d0693
Saqlain Ahmad <saqlain@ilmiya.com>
2026-07-06 13:11:29 +0500

    chat removed

--- src/routes/chat.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {chatController} = require('../controllers');
-const { authMiddleware } = require('../middleware/authmiddleware')
-
-router.route('/').post( chatController.sendMessage);
-module.exports = router;
